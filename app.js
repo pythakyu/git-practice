9b07bf162b3124bba73719d34506b4dcbe04e902
김동규 <pythakyu@gmail.com>
2022-12-28 18:30:45 +0900

    Add: app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+app.post("/users/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  return await myDataSource.query(
+    `
+      INSERT INTO
+        users (
+          username,
+          email,
+          password			
+        )
+      VALUES (
+        ?,
+        ?,
+        ?
+      )
+    `,
+    [username, email, password]
+  );
+});
